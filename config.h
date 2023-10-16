6afb429981cec5a882aceb04e94a6e3c2f4c98a8
Mike Vink <mike1994vink@gmail.com>
2023-10-16 23:26:12 +0200

    add tiny irc client

--- config.h
+++ config.h
@@ -214,7 +214,7 @@ static const Key keys[] = {
 	{ MODKEY,			XK_x,		incrgaps,	{.i = -3 } },
 	/* { MODKEY|ShiftMask,		XK_x,		spawn,		SHCMD("") }, */
 	{ MODKEY,			XK_c,		spawn,		{.v = (const char*[]){ TERMINAL, "-e", "profanity", NULL } } },
-	/* { MODKEY|ShiftMask,		XK_c,		spawn,		SHCMD("") }, */
+	{ MODKEY|ShiftMask,		XK_c,		spawn,		{.v = (const char*[]){ TERMINAL, "-e", "tiny", NULL } } },
 	/* V is automatically bound above in STACKKEYS */
 	{ MODKEY,			XK_b,		togglebar,	{0} },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
