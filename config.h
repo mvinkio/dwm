2133b7633e0ebaa6ee07bac9dd49016d84889b2d
Mike Vink <ivi@vinkies.net>
2023-10-24 10:16:13 +0200

    add wfica window

--- config.h
+++ config.h
@@ -50,6 +50,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	*/
 	/* class    instance      title       	 tags mask    isfloating   isterminal  noswallow  monitor */
+	{ "Wfica",     NULL,       NULL,       	    0,        0,           0,          0,         -1 },
 	{ "Gimp",     NULL,       NULL,       	    1 << 8,       0,           0,         0,        -1 },
 	{ TERMCLASS,  NULL,       NULL,       	    0,            0,           1,         0,        -1 },
 	{ NULL,       NULL,       "Event Tester",   0,            0,           0,         1,        -1 },
